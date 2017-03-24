22863e80224457e8c739993236e0799cb343231b
jeanphi <jeanphi@Jipito.local>
2017-03-24 14:39:04 -0400

    Added R code

---
+++ R/combat.R
@@ -0,0 +1,128 @@
+combat <- function(dat, batch, mod=NULL, par.prior=TRUE,prior.plots=FALSE) {
+  # make batch a factor and make a set of indicators for batch
+  if(length(dim(batch))>1){stop("This version of ComBat only allows one batch variable")}  ## to be updated soon!
+  batch <- as.factor(batch)
+  batchmod <- model.matrix(~-1+batch)  
+  cat("Found",nlevels(batch),'batches\n')
+  
+  # A few other characteristics on the batches
+  n.batch <- nlevels(batch)
+  batches <- list()
+  for (i in 1:n.batch){
+    batches[[i]] <- which(batch == levels(batch)[i])
+  } # list of samples in each batch  
+  n.batches <- sapply(batches, length)
+  n.array <- sum(n.batches)
+  
+  #combine batch variable and covariates
+  design <- cbind(batchmod,mod)
+  # check for intercept in covariates, and drop if present
+  check <- apply(design, 2, function(x) all(x == 1))
+  design <- as.matrix(design[,!check])
+  
+  # Number of covariates or covariate levels
+  cat("Adjusting for",ncol(design)-ncol(batchmod),'covariate(s) or covariate level(s)\n')
+  
+  # Check if the design is confounded
+  if(qr(design)$rank<ncol(design)){
+    if(ncol(design)==(n.batch+1)){
+      stop("The covariate is confounded with batch! Remove the covariate and rerun ComBat")
+    }
+    if(ncol(design)>(n.batch+1)){
+      if((qr(design[,-c(1:n.batch)])$rank<ncol(design[,-c(1:n.batch)]))){
+        stop('The covariates are confounded! Please remove one or more of the covariates so the design is not confounded')
+      } else {
+        stop("At least one covariate is confounded with batch! Please remove confounded covariates and rerun ComBat")
+      }
+    }
+  }
+    
+  
+  ##Standardize Data across genes
+  cat('Standardizing Data across genes\n')
+  B.hat <- solve(t(design)%*%design)%*%t(design)%*%t(as.matrix(dat))
+  
+      
+  #Standarization Model
+  grand.mean <- t(n.batches/n.array)%*%B.hat[1:n.batch,]
+  var.pooled <- ((dat-t(design%*%B.hat))^2)%*%rep(1/n.array,n.array)
+  stand.mean <- t(grand.mean)%*%t(rep(1,n.array))
+  if(!is.null(design)){
+    tmp <- design;tmp[,c(1:n.batch)] <- 0
+    stand.mean <- stand.mean+t(tmp%*%B.hat)
+  }	
+  s.data <- (dat-stand.mean)/(sqrt(var.pooled)%*%t(rep(1,n.array)))
+  
+  ##Get regression batch effect parameters
+  cat("Fitting L/S model and finding priors\n")
+  batch.design <- design[,1:n.batch]
+  gamma.hat <- solve(t(batch.design)%*%batch.design)%*%t(batch.design)%*%t(as.matrix(s.data))
+
+  delta.hat <- NULL
+  for (i in batches){
+    delta.hat <- rbind(delta.hat,apply(s.data[,i], 1, var,na.rm=T))
+  }
+  
+  ##Find Priors
+  gamma.bar <- apply(gamma.hat, 1, mean)
+  t2 <- apply(gamma.hat, 1, var)
+  a.prior <- apply(delta.hat, 1, aprior)
+  b.prior <- apply(delta.hat, 1, bprior)
+  
+  
+  ##Plot empirical and parametric priors
+  
+  if (prior.plots & par.prior){
+    par(mfrow=c(2,2))
+    tmp <- density(gamma.hat[1,])
+    plot(tmp,  type='l', main="Density Plot")
+    xx <- seq(min(tmp$x), max(tmp$x), length=100)
+    lines(xx,dnorm(xx,gamma.bar[1],sqrt(t2[1])), col=2)
+    qqnorm(gamma.hat[1,])	
+    qqline(gamma.hat[1,], col=2)	
+    
+    tmp <- density(delta.hat[1,])
+    invgam <- 1/rgamma(ncol(delta.hat),a.prior[1],b.prior[1])
+    tmp1 <- density(invgam)
+    plot(tmp,  typ='l', main="Density Plot", ylim=c(0,max(tmp$y,tmp1$y)))
+    lines(tmp1, col=2)
+    qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab='Theoretical Quantiles')	
+    lines(c(0,max(invgam)),c(0,max(invgam)),col=2)	
+    title('Q-Q Plot')
+  }
+  
+  ##Find EB batch adjustments
+  
+  gamma.star <- delta.star <- NULL
+  if(par.prior){
+    cat("Finding parametric adjustments\n")
+    for (i in 1:n.batch){
+        temp <- it.sol(s.data[,batches[[i]]],gamma.hat[i,],delta.hat[i,],gamma.bar[i],t2[i],a.prior[i],b.prior[i])
+        gamma.star <- rbind(gamma.star,temp[1,])
+        delta.star <- rbind(delta.star,temp[2,])
+    }
+  }else{
+    cat("Finding nonparametric adjustments\n")
+    for (i in 1:n.batch){
+      temp <- int.eprior(as.matrix(s.data[,batches[[i]]]),gamma.hat[i,],delta.hat[i,])
+      gamma.star <- rbind(gamma.star,temp[1,])
+      delta.star <- rbind(delta.star,temp[2,])
+    }
+  }
+  
+  
+  ### Normalize the Data ###
+  cat("Adjusting the Data\n")
+  
+  bayesdata <- s.data
+  j <- 1
+  for (i in batches){
+    bayesdata[,i] <- (bayesdata[,i]-t(batch.design[i,]%*%gamma.star))/(sqrt(delta.star[j,])%*%t(rep(1,n.batches[j])))
+    j <- j+1
+  }
+  
+  bayesdata <- (bayesdata*(sqrt(var.pooled)%*%t(rep(1,n.array))))+stand.mean
+  
+  return(bayesdata)
+  
+}
