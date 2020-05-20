ebda7095787b13efb98a4a004e3e3671244f3801
Jean-Philippe Fortin <fortin.jeanphilippe@gene.com>
2020-05-19 21:59:31 -0700

    Updated norm_pythin

--- Testing/norm_python.py
+++ Testing/norm_python.py
@@ -9,139 +9,54 @@ batch_col = 'batch'
 
 
 
+
+covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
+covars = pd.DataFrame(covars)   
 # Adjusted + Parametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-			batch_col=batch_col,
-                     categorical_cols=categorical_cols)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols)
 np.savetxt('outputData/testdata_combat_parametric_adjusted_python.csv', data_combat, delimiter=',')
-
-
-# Unadjusted + Parametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2]}
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-					 batch_col=batch_col)
-data_combat =np.transpose(data_combat)
-np.savetxt('outputData/testdata_combat_parametric_unadjusted_python.csv', data_combat, delimiter=',')
-
-
-
-# NoEB + adjusted + Parametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-					 batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     eb=False)
-data_combat =np.transpose(data_combat)
+# Adjusted + Parametric +NoEB
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,eb=False)
 np.savetxt('outputData/testdata_combat_parametric_adjusted_noeb_python.csv', data_combat, delimiter=',')
-
-# NoEB + Unadjusted + Parametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2]}
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-					 batch_col=batch_col,
-					 eb=False)
-data_combat =np.transpose(data_combat)
-np.savetxt('outputData/testdata_combat_parametric_unadjusted_noeb_python.csv', data_combat, delimiter=',')
-
-
-
-
 # Adjusted + NonParametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
-covars = pd.DataFrame(covars)   
-
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     parametric=False)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,parametric=False)
 np.savetxt('outputData/testdata_combat_nonparametric_adjusted_python.csv', data_combat, delimiter=',')
-
-
-
-
-# Unadjusted + NonParametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2]}
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     parametric=False)
-data_combat =np.transpose(data_combat)
-np.savetxt('outputData/testdata_combat_nonparametric_unadjusted_python.csv', data_combat, delimiter=',')
-
-
-
-# NoEB + adjusted + NonParametric
-covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                                    batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     eb=False,
-                     parametric=False)
-data_combat =np.transpose(data_combat)
+# Adjusted + NonParametric + NoEB
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,eb=False,parametric=False)
 np.savetxt('outputData/testdata_combat_nonparametric_adjusted_noeb_python.csv', data_combat, delimiter=',')
 
-# NoEB + Unadjusted + NonParametric
+
+
 covars = {'batch':[1,1,1,1,1,2,2,2,2,2]}
-covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                                    batch_col=batch_col,
-                                    eb=False,
-                                    parametric=False)
-data_combat =np.transpose(data_combat)
+covars = pd.DataFrame(covars) 
+# Unadjusted + Parametric
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col)
+np.savetxt('outputData/testdata_combat_parametric_unadjusted_python.csv', data_combat, delimiter=',')
+# Unadjusted + Parametric + NoEB
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,eb=False)
+np.savetxt('outputData/testdata_combat_parametric_unadjusted_noeb_python.csv', data_combat, delimiter=',')
+# Unadjusted + NonParametric
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,parametric=False)
+np.savetxt('outputData/testdata_combat_nonparametric_unadjusted_python.csv', data_combat, delimiter=',')
+# Unadjusted + NonParametric + NoEB 
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,eb=False,parametric=False)
 np.savetxt('outputData/testdata_combat_nonparametric_unadjusted_noeb_python.csv', data_combat, delimiter=',')
+
+
+
 
 
 ######## Mean only True
 # Adjusted + Parametric
 covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
 covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,mean_only=True)
 np.savetxt('outputData/testdata_combat_parametric_adjusted_meanonly_python.csv', data_combat, delimiter=',')
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     parametric=False,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,parametric=False,mean_only=True)
 np.savetxt('outputData/testdata_combat_nonparametric_adjusted_meanonly_python.csv', data_combat, delimiter=',')
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     parametric=True,
-                     eb=False,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,parametric=True,eb=False,mean_only=True)
 np.savetxt('outputData/testdata_combat_parametric_adjusted_noeb_meanonly_python.csv', data_combat, delimiter=',')
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     categorical_cols=categorical_cols,
-                     parametric=False,
-                     eb=False,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,categorical_cols=categorical_cols,parametric=False,eb=False,mean_only=True)
 np.savetxt('outputData/testdata_combat_nonparametric_adjusted_noeb_meanonly_python.csv', data_combat, delimiter=',')
 
 
@@ -149,34 +64,13 @@ np.savetxt('outputData/testdata_combat_nonparametric_adjusted_noeb_meanonly_pyth
 # UnAdjusted + Parametric
 covars = {'batch':[1,1,1,1,1,2,2,2,2,2], 'gender':[1,2,1,2,1,2,1,2,1,2]} 
 covars = pd.DataFrame(covars)   
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data, covars=covars,batch_col=batch_col,mean_only=True)
 np.savetxt('outputData/testdata_combat_parametric_unadjusted_meanonly_python.csv', data_combat, delimiter=',')
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     parametric=False,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,parametric=False,mean_only=True)
 np.savetxt('outputData/testdata_combat_nonparametric_unadjusted_meanonly_python.csv', data_combat, delimiter=',')
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     parametric=True,
-                     eb=False,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,parametric=True,eb=False,mean_only=True)
 np.savetxt('outputData/testdata_combat_parametric_unadjusted_noeb_meanonly_python.csv', data_combat, delimiter=',')
-data_combat = neuroCombat(dat=data,
-                     covars=covars,
-                     batch_col=batch_col,
-                     parametric=False,
-                     eb=False,
-                     mean_only=True)
-data_combat =np.transpose(data_combat)
+data_combat = neuroCombat(dat=data,covars=covars,batch_col=batch_col,parametric=False,eb=False,mean_only=True)
 np.savetxt('outputData/testdata_combat_nonparametric_unadjusted_noeb_meanonly_python.csv', data_combat, delimiter=',')
 
 
