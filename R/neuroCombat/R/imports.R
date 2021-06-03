d4ca0c1fdf1c0118d151846a8db4cdd0becebde9
Jean-Philippe Fortin <fortin.jeanphilippe@gene.com>
2021-06-03 08:32:16 -0700

    Added neuroCombat R package as a submodule

--- R/neuroCombat/R/imports.R
+++
@@ -1,5 +0,0 @@
-#' @importFrom matrixStats rowVars rowSds
-#' @importFrom matrixStats rowMeans2 rowSums2
-#' @importFrom stats rnorm rgamma var density model.matrix median
-#' @importFrom graphics lines plot
-NULL
